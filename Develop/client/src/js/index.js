a338fd5fc6a1f0aa1b721adc8ffed263f637b37f
abstractl0gic <zwlyons@gmail.com>
2023-10-01 18:43:39 -0500

    add commenting to index.js

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -1,31 +1,35 @@
+// Import necessary modules and styles
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
 
+// Get the main container element from the DOM
 const main = document.querySelector('#main');
-main.innerHTML = '';
 
+// Function to show loading spinner while editor is loading
 const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
   spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
   `;
   main.appendChild(spinner);
 };
 
+// Create a new instance of the Editor class
 const editor = new Editor();
 
+// Check if editor is loaded, if not, display loading spinner
 if (typeof editor === 'undefined') {
   loadSpinner();
 }
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
+  // Register Workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
