59126719d90213512abda4e80d48fa30f2b83e49
abstractl0gic <zwlyons@gmail.com>
2023-10-01 14:01:05 -0500

    configure webpack to handle css

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,28 +3,45 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new WebpackPwaManifest({
+      name: 'PWA Text Editor',
+      short_name: 'JATE',
+      description: 'A simple lightweight web-based text editor!',
+      background_color: '#ffffff',
+      theme_color: '#31a9e1',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons'),
+        },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
