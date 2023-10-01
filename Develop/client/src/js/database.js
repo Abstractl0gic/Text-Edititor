88c62ce0288725c3426a38351a69ee3ffa20d226
abstractl0gic <zwlyons@gmail.com>
2023-10-01 17:45:22 -0500

    add event listener logic install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,27 +1,33 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+// Initialize the database
+const initdb = async () => {
+  const db = await openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+      if (!db.objectStoreNames.contains('jate')) {
+        // Create the object store if it doesn't exist
+        const store = db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        // Add an index for timestamp if needed
+        store.createIndex('timestamp', 'timestamp');
+        console.log('Created Databse!');
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+  return db;
+};
 
+// Add content to the database
 export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
 
-  // Use add() method to add data to the object store
+  // Use add() method to add data to the object store with a timestamp
   await store.add({ content, timestamp: Date.now() });
   await tx.done;
 };
 
+// Get all content from the database
 export const getDb = async () => {
   const db = await initdb();
   const tx = db.transaction('jate', 'readonly');
@@ -30,6 +36,7 @@ export const getDb = async () => {
   // Use getAll() method to retrieve all data from the object store
   const content = await store.getAll();
   await tx.done;
-
   return content;
 };
+
+initdb(); // Call initdb to ensure the database is initialized when the module is imported
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,45 +3,45 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/js/index.js',
-    install: './src/js/install.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-    new WebpackPwaManifest({
-      name: 'PWA Text Editor',
-      short_name: 'JATE',
-      description: 'A simple lightweight web-based text editor!',
-      background_color: '#ffffff',
-      theme_color: '#31a9e1',
-      icons: [
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
+    ],
+    module: {
+      rules: [
         {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('icons'),
+          test: /\.js$/, 
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/, 
+          use: ['style-loader', 'css-loader'],
         },
       ],
-    }),
-    new InjectManifest({
-      swSrc: './src-sw.js',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+    },
+  };
 };
