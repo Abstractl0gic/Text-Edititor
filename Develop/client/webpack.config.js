d9643675ce8260afbfaac48206ae023a1a6b3933
abstractl0gic <zwlyons@gmail.com>
2023-10-01 17:50:58 -0500

    add entry points for editor, database, and header to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -8,7 +8,10 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',  // Added editor entry point
+      database: './src/js/database.js',  // Added database entry point
+      header: './src/js/header.js',  // Added header entry point
     },
     output: {
       filename: '[name].bundle.js',
@@ -20,6 +23,20 @@ module.exports = () => {
         filename: 'index.html',
       }),
       new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'JavaScript Text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
